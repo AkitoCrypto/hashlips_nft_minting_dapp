16356591e721ca2fff0a7d74f50bfe4ff5c7425e
Ryanblwang <87690606+Ryanblwang@users.noreply.github.com>
2022-09-14 21:34:35 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ function App() {
   const blockchain = useSelector((state) => state.blockchain);
   const data = useSelector((state) => state.data);
   const [claimingNft, setClaimingNft] = useState(false);
-  const [feedback, setFeedback] = useState(`https://twitter.com/themergecat`);
+  const [feedback, setFeedback] = useState(`Twitter @themergecat`);
   const [mintAmount, setMintAmount] = useState(1);
   const [CONFIG, SET_CONFIG] = useState({
     CONTRACT_ADDRESS: "",
