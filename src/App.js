24f50fb9ef69c61d150f549791ea423d2c8f67ca
Ryanblwang <87690606+Ryanblwang@users.noreply.github.com>
2022-11-14 22:38:49 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -205,7 +205,7 @@ function App() {
         <s.SpacerSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
           <s.Container flex={1} jc={"center"} ai={"center"}>
-            <StyledImg alt={"example"} src={"/config/images/example.gif"} />
+            <StyledImg alt={"testexample"} src={"/config/images/testexample.png"} />
           </s.Container>
           <s.SpacerLarge />
           <s.Container
@@ -372,8 +372,8 @@ function App() {
           <s.SpacerLarge />
           <s.Container flex={1} jc={"center"} ai={"center"}>
             <StyledImg
-              alt={"example"}
-              src={"/config/images/example.gif"}
+              alt={"testexample"}
+              src={"/config/images/testexample.png"}
               style={{ transform: "scaleX(-1)" }}
             />
           </s.Container>
